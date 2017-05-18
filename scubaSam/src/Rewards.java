a26084b295ea3fcd413a3f5f3eee1a0a8ac89e17
1828995 <1828995@00-46239.pusd.dom>
2017-05-18 14:37:28 -0700

    Rewards Interface

--- scubaSam/src/Rewards.java
+++ scubaSam/src/Rewards.java
@@ -1,4 +1,10 @@
+import java.awt.Point;
 
-public interface Rewards {
 
+public interface Rewards 
+{
+	public Point move();
+	public Point getLocation();
+	public boolean canMove();
+	public int getValue();
 }
